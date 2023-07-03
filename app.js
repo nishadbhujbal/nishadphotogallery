9b99a8c397ab37ada118b8638140d8de624c173f
nishadbhujbal <127653267+nishadbhujbal@users.noreply.github.com>
2023-07-03 16:55:37 +0530

    added the final app.js fule

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+// Express specific stuff
+app.use('/static', express.static('static'))
+app.use(express.urlencoded())
+
+// Pug specific stuff
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+// Endpoints
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('home.pug');
+})
+
+app.get('/nishad', (req, res) => {
+    const params = {}
+    res.status(200).render('nishad.pug');
+})
+
+app.get('/football', (req, res) => {
+    const params = {}
+    res.status(200).render('football.pug');
+})
+
+app.get('/vishv', (req, res) => {
+    const params = {}
+    res.status(200).render('vishv.pug');
+})
+
+app.get('/pets', (req, res) => {
+    const params = {}
+    res.status(200).render('pets.pug');
+})
+
+// Start the server
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
